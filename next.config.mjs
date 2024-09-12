98a364c40f7633b08273e91718ccef8866294991
gd7329 <gyasuddin@ajnalens.com>
2024-09-12 23:20:08 +0530

    new changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  pageExtensions: ["js", "jsx", "ts", "tsx"],
+
+  // Export trailing slashes (optional, depending on your hosting provider)
+  trailingSlash: true,
+  eslint: {
+    dirs: ["src"],
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
